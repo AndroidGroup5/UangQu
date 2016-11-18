4906752d52f99b190728bf33d8ce9c608404c4f2
Faiza Aulia Rahma Putra <faizaaulia20@gmail.com>
2016-11-18 19:44:54 +0700

    Change Icon, Add Input Activity

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl405142332/InputData.java
@@ -0,0 +1,19 @@
+package id.sch.smktelkom_mlg.project.xirpl405142332;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import id.sch.smktelkom_mlg.project.xirpl405142332.uangqu.R;
+
+
+public class InputData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTitle("Add Record");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_data);
+
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl405142332/uangqu/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl405142332/uangqu/MainActivity.java
@@ -1,7 +1,6 @@
 package id.sch.smktelkom_mlg.project.xirpl405142332.uangqu;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -9,21 +8,14 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+
+import id.sch.smktelkom_mlg.project.xirpl405142332.InputData;
 
 
-public class MainActivity extends id.sch.smktelkom_mlg.project.xirpl405142332.uangqu.ALockingClass
-        implements NavigationView.OnNavigationItemSelectedListener {
-
-    AlertDialog.Builder dialog;
-    View dialogView;
-    LayoutInflater inflater;
-    EditText txt_id, txt_nama, txt_alamat;
-    String id, nama, alamat;
+public class MainActivity extends ALockingClass implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +30,7 @@ public class MainActivity extends id.sch.smktelkom_mlg.project.xirpl405142332.ua
 
             @Override
             public void onClick(View view) {
-                DialogForm("", "", "", "SIMPAN");
+                goAdd();
             }
 
         });
@@ -53,60 +45,8 @@ public class MainActivity extends id.sch.smktelkom_mlg.project.xirpl405142332.ua
         navigationView.setNavigationItemSelectedListener(this);
         }
 
-    private void DialogForm(String idx, String namax, String alamatx, String button) {
-        dialog = new AlertDialog.Builder(MainActivity.this);
-        inflater = getLayoutInflater();
-        dialogView = inflater.inflate(R.layout.form_biodata, null);
-        dialog.setView(dialogView);
-        dialog.setCancelable(true);
-        dialog.setIcon(R.mipmap.ic_launcher);
-        dialog.setTitle("Form Biodata");
-
-        txt_id = (EditText) dialogView.findViewById(R.id.txt_id);
-        txt_nama = (EditText) dialogView.findViewById(R.id.txt_nama);
-        txt_alamat = (EditText) dialogView.findViewById(R.id.txt_alamat);
-
-        if (!idx.isEmpty()) {
-            txt_id.setText(idx);
-            txt_nama.setText(namax);
-            txt_alamat.setText(alamatx);
-        } else {
-            kosong();
-        }
-
-        dialog.setPositiveButton(button, new DialogInterface.OnClickListener() {
-
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                id = txt_id.getText().toString();
-                nama = txt_nama.getText().toString();
-                alamat = txt_alamat.getText().toString();
-
-                simpan_update();
-                dialog.dismiss();
-            }
-        });
-
-        dialog.setNegativeButton("BATAL", new DialogInterface.OnClickListener() {
-
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-                kosong();
-            }
-        });
-
-        dialog.show();
-    }
-
-    private void simpan_update() {
-
-    }
-
-    private void kosong() {
-        txt_id.setText(null);
-        txt_nama.setText(null);
-        txt_alamat.setText(null);
+    private void goAdd() {
+        startActivity(new Intent(this, InputData.class));
     }
 
     @Override
