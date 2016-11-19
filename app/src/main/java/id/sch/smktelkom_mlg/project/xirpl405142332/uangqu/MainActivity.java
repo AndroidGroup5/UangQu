50c952b0c2e58df2621614c69c619b52520f1797
Faiza Aulia Rahma Putra <faizaaulia20@gmail.com>
2016-11-19 09:48:55 +0700

    Fix Input Activity, Remove Navigation Drawer

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl405142332/uangqu/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl405142332/uangqu/MainActivity.java
@@ -34,16 +34,8 @@ public class MainActivity extends ALockingClass implements NavigationView.OnNavi
             }
 
         });
+}
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        }
 
     private void goAdd() {
         startActivity(new Intent(this, InputData.class));
